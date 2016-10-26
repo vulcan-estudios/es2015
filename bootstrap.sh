93a179ee681185a404a7de5256ab729cf4d8f7fc
Romel Pérez <ronelprhone@gmail.com>
2016-10-26 18:18:06 -0500

    [chore] vagrant setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,5 @@
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-## MongoDB
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.3 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get update
-sudo apt-get install mongodb
+## Packages
+cd /vagrant/ && npm install
